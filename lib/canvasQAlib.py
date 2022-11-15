61b460c77470c3b49147c700d002a80350c22f07
Steven <s.booten@griffith.edu.au>
2022-11-16 08:42:09 +1000

    update

---
+++ lib/canvasQAlib.py
@@ -0,0 +1,16 @@
+import logging
+
+def initialiseLogging(logFileName=None):
+    """Set up basic logging information for all CAR generation
+    Use default MIGRATION_LOGFILE unless filename specified
+    Specified log file is just a name used with MIGRATION_BASE
+    """
+
+    if logFileName is None:
+        logFileName = settings.MIGRATION_LOGFILE
+    else:
+        logFileName = f"{settings.MIGRATION_BASE}{logFileName}"
+
+    logging.basicConfig( level=logging.INFO,
+        format='%(asctime)s %(levelname)s: %(filename)s %(funcName)s %(lineno)d: %(message)s',
+        filename=logFileName )
